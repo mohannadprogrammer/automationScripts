ce1065af2bc390513124b94cf404bad77b529543
mohannadprogrammer <waheedmohannad55578@gmail.com>
2025-08-29 00:06:19 +0200

    [ADD] add script that pre requisites for docker

---
+++ docker_install.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+# Install Docker on Ubuntu
+
+# Update package index and install prerequisites
+sudo mkdir -p /etc/apt/keyrings
+
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# Set up the Docker repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null  
